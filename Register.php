62908d976582ecd18fc92a1850a7feeb3848945e
MiruRyuuki <71747975+MiruRyuuki@users.noreply.github.com>
2021-07-19 21:50:38 +0800

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BodyWork | Register</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="content" class="content">
+<?php
+if(isset($_GET['err'])){
+	?>
+	<div class="warning"><?php echo $_GET['err']?></div>
+	<?php
+}
+?>
+<?php
+if(isset($_GET['msg'])){
+	?>
+	<div class="message"><?php echo $_GET['msg']?></div>
+	<?php
+}
+?>
+
+<form action="doRegister.php" method="post" id="form_register" onsubmit="return validate();">
+<div class="form">
+		<table class="table">
+			<tr>
+				<th align="left">First Name</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<th align="left">Last Name</td>
+				<td><input type="text" name="lname" value=""></td>
+			</tr>
+			<tr>
+				<th align="left">Email</td>
+				<td><input type="text" name="email" value=""></td>
+			</tr>
+			<tr>
+				<th align="left">Password</td>
+				<td><input type="password" name="password" value=""></td>
+			</tr>
+			<tr>
+				<th align="left">Username</td>
+				<td><input type="text" name="username" value=""></td>
+			</tr>
+			<tr>
+				<th align="left">Gender</td>
+				<td><label><input type="radio" name="gender" value="Male" /> Male</label>
+				<label><input type="radio" name="gender" value="Female" /> Female</label></td>
+			</tr>
+			<tr>
+				<th align="left">Height</td>
+				<td><input type="text" name="height" value=""></td>
+			</tr>
+			<tr>
+				<th align="left">Weight</td>
+				<td><input type="text" name="weight" value=""></td>
+			</tr>
+			<tr>
+				<th colspan="2" align="left"><button type="submit" class="btn-form">Submit</button></td>
+			</tr>
+		</table>
+	</div>
+	
+</form>	
+</div>
+</body>
+<script type="text/javascript">
+	function validate(){
+        var fname = form_register.fname.value;	
+		var lname = form_register.lname.value;	
+		var email = form_register.email.value;		
+		var password = form_register.password.value;	
+		var username = form_register.username.value;	
+        var gender = form_register.gender.value;
+        var height = form_register.height.value;	
+        var weight = form_register.weight.value;
+		if(Fname=="" || lname=="" || email=="" || password=="" || username=="" || gender==""|| height=="" || weight==""){
+			alert("Please fill all required fields!");
+			return false;
+		}	
+		return true;
+	}
+</script>
+</html> 
---
+++ db_bodywork.sql
@@ -0,0 +1,45 @@
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL
+  `gender` varchar(255) NOT NULL
+  `height` varchar(255) NOT NULL
+  `weight` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `fname`, `lname`, `email`, `password`, `username`, `gender`, `height`, `weight`) VALUES
+(1, 'Loly', 'Grande', 'lolyg@gmail.com', '5EB319F2B34565D65F4DB735DE522575', 'lolyg', 'Female', '165', '47');
+
+--
+-- Indexes for dumped tables
+--
+
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
