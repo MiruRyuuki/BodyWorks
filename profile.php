d7e586c8636bb036cd0b841b6581596cebb9f648
alessa03 <74395036+alessa03@users.noreply.github.com>
2021-07-22 21:03:13 +0700

    Update and rename profile.html to profile.php

---
+++ profile.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>BodyWorks | Profile</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="#">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<nav class="navbar navbar-expand d-flex flex-column align-item-start" id="sidebar">
+		<a href="#" class="navbar-brand">
+			<div>
+			<img height="200" src="images/BodyWorks_Logo.png" alt="BodyWorks Logo">
+			</div>
+		</a>
+		<ul class="navbar-nav d-flex flex-column mt-5 w-100">
+			<li class="nav-item w-100">
+				<a href="#" class="nav-link text-light pl-4">Home</a>
+			</li>
+			<li class="nav-item w-100">
+				<a href="aboutUs.php" class="nav-link text-light pl-4">About Us</a>
+			</li>
+			<li class="nav-item w-100">
+				<a href="#" class="nav-link text-light pl-4">Blog</a>
+			</li>
+			<li class="nav-item dropdown w-100">
+				<a href="#" class="nav-link dropdown-toggle text-light pl-4" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">Service</a>
+				<ul class="dropdown-menu w-100" aria-labelledby="navbarDropdown">
+					<li><a href="#" class="dropdown-item text-light pl-4 p-2">Service 01</a></li>
+					<li><a href="#" class="dropdown-item text-light pl-4 p-2">Service 02</a></li>
+					<li><a href="#" class="dropdown-item text-light pl-4 p-2">Service 03</a></li>
+				</ul>
+			</li>
+			<li class="nav-item w-100">
+				<a href="contactUs.php" class="nav-link text-light pl-4 ">Contact Us</a>
+			</li>
+			<hr>
+			<li class="nav-item w-100">
+				<a href="profile.php" class="nav-link text-light pl-4 active">Profile</a>
+			</li>
+		</ul>
+    </nav>
+
+<link rel="stylesheet" type="text/css" href="profile.css">
+<div class="bg-profile100" style="background-image: url('images/gym-pro.jpg');">
+    <section class="mycontainer">
+
+        <body>
+            <article class="title" style="text-align: left;">
+                <h2>Welcome to My Profile</h2>
+            </article>
+
+            <div class="profile" style="max-width: 900px; margin: 3em auto">
+                <table width="100%">
+                    <tbody>
+                    <tr><td>First Name</td><td>:</td><td>Yael</td></tr>
+                    <tr><td></br>Last Name</td><td></br>:</td><td></br>Shelbia</td></tr>
+                    <tr><td></br>Gender</td><td></br>:</td><td></br>Female</td></tr>
+                    <tr><td></br>Username</td><td></br>:</td><td></br>yaels03</td></tr>
+                    <tr><td></br>Gender</td><td></br>:</td><td></br>yaelshelbia03@gmail.com</td></tr>
+                    <tr><td></br>Height</td><td></br>:</td><td></br>167 cm</td></tr>
+                    <tr><td></br>Weight</td><td></br>:</td><td></br>50 kg</td></tr>
+                    </tbody></table>
+            </div>
+        </body>
+    </section>
+</div>
